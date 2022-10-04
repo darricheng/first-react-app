2c2f4b55275ec6047d30bce8116a2d1c18ffccab
Darric Heng <darricheng@gmail.com>
2022-10-04 22:00:41 +0800

    Change indentation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import "./App.css";
 
 const testing = {
-    firstName: "John",
-    lastName: "Lee",
+  firstName: "John",
+  lastName: "Lee",
 };
 const testing2 = {
-    hobby: "bowling",
+  hobby: "bowling",
 };
 
 function App() {
-    const total = { name: testing, details: testing2 };
-    return (
-        <div className="App">
-            <h1 id="page-title">Hello World</h1>
-            <Text {...total} />
-        </div>
-    );
+  const total = { name: testing, details: testing2 };
+  return (
+    <div className="App">
+      <h1 id="page-title">Hello World</h1>
+      <Text {...total} />
+    </div>
+  );
 }
 
 function Text(props) {
-    return (
-        <p>
-            {props.name.firstName} likes {props.details.hobby}.
-        </p>
-    );
+  return (
+    <p>
+      {props.name.firstName} likes {props.details.hobby}.
+    </p>
+  );
 }
 
 export default App;
